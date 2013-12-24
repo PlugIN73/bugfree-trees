ce8fdf0574704321888d00607753b100f2f698b8
plugin73 <laplugin73@gmail.com>
2013-12-24 20:47:13 +0100

    test for difficult cases

--- tree_test.rb
+++ tree_test.rb
@@ -30,4 +30,29 @@ class TestTree < Minitest::Test
     assert {@tree.node.left.value == 4}
   end
 
+  def test_should_insert_same_element_on_left_leaf
+    @tree.insert 5
+    @tree.insert 5
+
+    assert {@tree.node.value == 5}
+    assert {@tree.node.left.value == 5}
+  end
+
+  def test_should_rigth_choose_leaf_on_difficult_insert
+    @tree.insert 5
+    @tree.insert 3
+    @tree.insert 7
+    @tree.insert 6
+
+    assert {@tree.node.right.left.value == 6}
+
+    @tree.insert 4
+
+    assert {@tree.node.left.right.value == 4}
+
+    @tree.insert 2
+
+    assert {@tree.node.left.left.value == 2}
+  end
+
 end
